ee9adc6790af7b536a83c8a842030f00c721b6b1
grzianod <grazianodinocca@icloud.com>
2023-03-12 12:18:06 +0100

    v1.2

--- es0/export.c
+++ es0/export.c
@@ -17,30 +17,52 @@ typedef struct {
 
 typedef struct {
     int type;
-    char message[21]; // stringa null terminated lung max 20
+    char message[21];
 } MessageStruct;
 
 typedef struct {
-        int type;
-        union {
-            ValueStruct val;
-            MValueStruct mvals;
-            MessageStruct messages; };
+    int type;
+    union {
+        ValueStruct val;
+        MValueStruct mvals;
+        MessageStruct messages; };
 } ExportData;
 
 void export(ExportData *data, int n, FILE *fp) {
     for(int i=0; i<n; i++)
-            fwrite(&data[i], sizeof(ExportData), 1, fp);
+        fwrite(&data[i], sizeof(ExportData), 1, fp);
 }
 
 int main() {
     ExportData export_data_arr[100];
     srand(time(NULL)); // seed the random number generator with current time
-
     FILE *fp;
 
+    char *strings[100] = {
+            "Artificial heart",
+            "Space exploration",
+            "Quantum computing",
+            "Renewable energy",
+            "Virtual reality",
+            "AI",
+            "Robotics technology",
+            "Neuroscience research",
+            "Genetic engineering",
+            "3D printing",
+            "Smart cities",
+            "Blockchain technology",
+            "Augmented reality",
+            "Nanotechnology",
+            "Clean water",
+            "Green energy",
+            "Autonomous vehicles",
+            "Wireless power",
+            "Smart homes",
+            "Advanced materials",
+    };
+
     if((fp = fopen("exported_data.bin", "wb+")) == NULL) {
-        printf("Error opening file \"data.bin\".");
+        printf("Error opening file \"exported_data.bin\".");
         exit(-1);
     }
 
@@ -52,25 +74,22 @@ int main() {
         switch (type) {
             case 1:
                 export_data.val.type = type;
-                export_data.val.val = (float) rand() / RAND_MAX * 100;
+                export_data.val.val = (float) rand() / (RAND_MAX * 100);
                 export_data.val.timestamp = time(NULL);
                 break;
             case 2:
                 export_data.mvals.type = type;
                 export_data.mvals.timestamp = time(NULL);
                 for (int j = 0; j < 10; j++) {
-                    export_data.mvals.val[j] = (float) rand() / RAND_MAX * 100;
+                    export_data.mvals.val[j] = (float) (rand() / RAND_MAX * 100);
                 }
                 break;
             case 3:
                 export_data.messages.type = type;
-                char message[21];
-                int len = rand() % 20 + 1; // generate random message length between 1 and 20
-                for (int j = 0; j < len; j++) {
-                    message[j] = 'a' + rand() % 26; // generate random lowercase letter
-                }
-                message[len] = '\0'; // add null terminator to message string
-                strncpy(export_data.messages.message, message, sizeof(export_data.messages.message));
+                char *message = strings[(int) (rand() % 20)];
+                int len = strlen(message);
+                strncpy(export_data.messages.message, strings[(int) (rand() % 20)], len);
+                export_data.messages.message[len] = '\0';
                 break;
         }
 
