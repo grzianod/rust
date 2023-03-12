50aab6ee638c5a3e67896857958cc2e16edfcbbd
grzianod <grazianodinocca@icloud.com>
2023-03-12 15:40:18 +0100

    v1.0

--- es0/e0/src/main.rs
+++ es0/e0/src/main.rs
@@ -1,3 +1,32 @@
+struct ValueStruct {
+    _type: i32,
+    val: f32,
+    timestamp: i64,
+}
+
+struct MValueStruct {
+    _type: i32,
+    val: [ f32; 10 ],
+    timestamp: i64,
+}
+
+struct MessageStruct {
+    _type: i32,
+    message: [u8; 21],
+}
+
+union ValueUnion {
+    val: ValueStruct,
+    mvals: MValueStruct,
+    messages: MessageStruct,
+}
+
+struct ExportData {
+    type_: i32,
+    value_union: ValueUnion,
+}
+
+
 fn main() {
 
 }
